a58c9d74204a6cfbb4b7a4bcfded433823a3dd3b
jkpraja <jati.k.praja@gmail.com>
2023-02-16 09:49:18 +0000

    add some modification

--- blogx/ubahenv.sh
+++ blogx/ubahenv.sh
@@ -3,4 +3,4 @@
 sed -i -e '/DB_DATABASE/s/laravel/blogx/g' .env
 sed -i -e '/DB_USERNAME/s/root/blogx/g' .env
 sed -i -e 's/DB_PASSWORD=/DB_PASSWORD=blogx/g' .env
-sed -i -e '/DB_HOST/s/127.0.0.1/mysql/g' .env
+#sed -i -e '/DB_HOST/s/127.0.0.1/mysql/g' .env
