cc7e82d89ae227d5e9a3e75d4ce328641763e802
jkpraja <jati.k.praja@gmail.com>
2023-02-20 15:10:44 +0000

    change db connection

--- blogx/ubahenv.sh
+++ blogx/ubahenv.sh
@@ -4,3 +4,4 @@ sed -i -e '/DB_DATABASE/s/laravel/blogx/g' .env
 sed -i -e '/DB_USERNAME/s/root/blogx/g' .env
 sed -i -e 's/DB_PASSWORD=/DB_PASSWORD=adminadmin1/g' .env
 sed -i -e '/DB_HOST/s/127.0.0.1/database/g' .env
+sed -i -e '/DB_CONNECTION/s/mysql/database/g' .env
