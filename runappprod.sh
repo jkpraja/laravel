68cfffbf92867b8789ccf61905e46ab8e5a49cb1
jkpraja <jati.k.praja@gmail.com>
2023-02-24 06:31:03 +0700

    fix bash script

--- runappprod.sh
+++ runappprod.sh
@@ -2,10 +2,15 @@
 
 if (docker container inspect -f '{{.State.Running}}' mysql-db) then
     echo "database is already running"
-    docker compose up -d web-server
+    if (docker container inspect -f '{{.State.Running}}' web-server) then
+        echo "web server is already running"
+    else
+        docker compose up -d laravel-server
+    fi
+    
     docker compose up -d laravel-app
 else
     docker compose up -d database
-    docker compose up -d web-server
+    docker compose up -d laravel-server
     docker compose up -d laravel-app
 fi
