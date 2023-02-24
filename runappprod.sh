f94e812557daa994d357cb8aacdd47bd7bc759e6
jkpraja <jati.k.praja@gmail.com>
2023-02-24 09:40:02 +0700

    fix supervisor

--- runappprod.sh
+++ runappprod.sh
@@ -2,15 +2,14 @@
 
 if (docker container inspect -f '{{.State.Running}}' mysql-db); then
     echo "database is already running"
-    if (docker container inspect -f '{{.State.Running}}' web-server) && (docker container inspect -f '{{.State.Status}}' web-server != 'Exited'); then
+    docker compose -f prod-compose.yaml up -d laravel-app
+    if (docker container inspect -f '{{.State.Running}}' web-server) && [docker container inspect -f '{{.State.Status}}' web-server != 'Exited']; then
         echo "web server is already running"
     else
         docker compose -f prod-compose.yaml up -d web-server
     fi
-    
-    docker compose -f prod-compose.yaml up -d laravel-app
 else
     docker compose -f prod-compose.yaml up -d database
-    docker compose -f prod-compose.yaml up -d web-server
     docker compose -f prod-compose.yaml up -d laravel-app
+    docker compose -f prod-compose.yaml up -d web-server
 fi
