a34bc81627775e31672bba9f6e1c641881941fa3
jkpraja <jati.k.praja@gmail.com>
2023-02-27 00:37:37 +0700

    add new processes to production script

--- runappprod.sh
+++ runappprod.sh
@@ -3,6 +3,7 @@
 if (docker container inspect -f '{{.State.Running}}' mysql-db); then
     echo "database is already running"
     docker compose -f prod-compose.yaml up -d laravel-app
+    
     if (docker container inspect -f '{{.State.Running}}' web-server) && [docker container inspect -f '{{.State.Status}}' web-server != 'Exited']; then
         echo "web server is already running"
     else
@@ -12,4 +13,6 @@ else
     docker compose -f prod-compose.yaml up -d database
     docker compose -f prod-compose.yaml up -d laravel-app
     docker compose -f prod-compose.yaml up -d web-server
+    docker exec -u 0 laravel-app php artisan key:generate
+    docker exec -u 0 laravel-app php artisan migrate
 fi
